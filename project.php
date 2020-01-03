14c597fe6be27e71f4493b7d872022e512c654a3
Muhammad Taufik Prayitno <taufikprayitno16@gmail.com>
2020-01-03 10:05:14 +0700

    Day 2  : add card content

--- project.php
+++ project.php
@@ -1,28 +1,35 @@
-<!--
-	Author: W3layouts
-	Author URL: http://w3layouts.com
-	License: Creative Commons Attribution 3.0 Unported
-	License URL: http://creativecommons.org/licenses/by/3.0/
--->
 <!DOCTYPE html>
 <html>
 <!-- Head -->
+
 <head>
 	<title>PT. Patraland</title>
 	<!-- Meta-Tags -->
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="keywords" content="Premier Realty a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
-		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="keywords" content="Premier Realty a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
+	<script type="application/x-javascript">
+		addEventListener("load", function() {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
 	<!-- //Meta-Tags -->
 	<!-- Custom-Theme-Files -->
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
 	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
 	<link rel="stylesheet" type="text/css" href="css/component.css" />
-	<!-- //Custom-Theme-Files -->
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css" />
+	<link rel="stylesheet" href="css/cards-gallery.css" <!-- //Custom-Theme-Files -->
 	<!-- Web-Fonts -->
-	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" 	type="text/css">
-	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat:400,700" 				type="text/css">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" type="text/css">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat:400,700" type="text/css">
+	<link rel="stylesheet" href="css/card.css">
 	<!-- //Web-Fonts -->
 	<!-- Default-JavaScript-File -->
 	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
@@ -31,11 +38,12 @@
 </head>
 <!-- //Head -->
 <!-- Body -->
+
 <body>
 	<!-- Header -->
 	<div class="header w3layouts-1">
 		<!-- Top-Bar -->
-			<?php include("includes/topbar.php"); ?>
+		<?php include("includes/topbar.php"); ?>
 		<!-- //Top-Bar -->
 		<div class="banner">
 			<div class="bann-info">
@@ -45,126 +53,93 @@
 	</div>
 	<!-- //Header -->
 	<div class="services wthree-3">
-				<div class="container">
-					<h2 class="tittle">Project</h2>
-					<div class="services-grids">
-						<div class="col-md-6 services-grid">
-							<div class="col-md-5 serv-img mask">
-								<img src="images/i2.jpg" alt="" class="img-responsive zoom-img">
-							</div>
-							<div class="col-md-7 serv-text">
-								<h4>Patra Garden</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,sed do eiusmod tempor incididunt .</p>
-							</div>
-						  <div class="clearfix"> </div>
-						</div>
-						<div class="col-md-6 services-grid">
-							<div class="col-md-5 serv-img mask">
-								<img src="images/i7.jpg" alt="" class="img-responsive zoom-img">
-							</div>
-							<div class="col-md-7 serv-text">
-								<h4>Patra Mulia Kencana</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,sed do eiusmod tempor incididunt .</p>
-							</div>
-						</div>
-					   <div class="clearfix"> </div>
-					</div>
-          <div class="services-grids">
-						<div class="col-md-6 services-grid">
-							<div class="col-md-5 serv-img mask">
-								<img src="images/i2.jpg" alt="" class="img-responsive zoom-img">
-							</div>
-							<div class="col-md-7 serv-text">
-								<h4>Patraland Griya Madani</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,sed do eiusmod tempor incididunt .</p>
-							</div>
-						  <div class="clearfix"> </div>
-						</div>
-						<div class="col-md-6 services-grid">
-							<div class="col-md-5 serv-img mask">
-								<img src="images/i7.jpg" alt="" class="img-responsive zoom-img">
-							</div>
-							<div class="col-md-7 serv-text">
-								<h4>Patra Mulia Kencana</h4>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,sed do eiusmod tempor incididunt .</p>
-							</div>
-						</div>
-					   <div class="clearfix"> </div>
-					</div>
+		<div class="container">
+			<h2 class="tittle">Project</h2>
+			<div class="card">
+				<img src="images/image1.jpg" alt="Avatar" style="width:100%">
+				<div class="container-card">
+					<h4><b>Patra Garden</b></h4>
+					<p>Kata kata patra garden</p>
 				</div>
 			</div>
-			<div class="featured-section">
-				<div class="container">
-					<h3 class="tittle">Featured services</h3>
-					<div class="featured-grids w3ls-2">
-						<div class="col-md-3 agile-3">
-							<div class="icon hi-icon-wrap hi-icon-effect-6">
-								<i><a href="#" class="hi-icon hi-icon-cog glyphicon glyphicon-user"></a></i>
-							</div>
-							<h4>Accusantium</h4>
-							<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
-						</div>
-						<div class="col-md-3 agile-3">
-							<div class="icon hi-icon-wrap hi-icon-effect-6">
-								<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-book"></a>
-							</div>
-							<h4>Lorem ipsum</h4>
-							<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
-						</div>
-						<div class="col-md-3 agile-3">
-							<div class="icon hi-icon-wrap hi-icon-effect-6">
-								<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-globe"></a>
-							</div>
-							<h4>adipiscing elit</h4>
-							<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
-						</div>
-						<div class="col-md-3 agile-3">
-							<div class="icon hi-icon-wrap hi-icon-effect-6">
-								<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-wrench"></a>
-							</div>
-							<h4>Accusantium</h4>
-							<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
-						</div>
-						<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+	</div>
+	</div>
+	<div class="featured-section">
+		<div class="container">
+			<h3 class="tittle">Featured services</h3>
+			<div class="featured-grids w3ls-2">
+				<div class="col-md-3 agile-3">
+					<div class="icon hi-icon-wrap hi-icon-effect-6">
+						<i><a href="#" class="hi-icon hi-icon-cog glyphicon glyphicon-user"></a></i>
 					</div>
-					<div class="featured-grids w3l-2">
-						<div class="col-md-3 agile-3">
-							<div class="icon hi-icon-wrap hi-icon-effect-6">
-								<i><a href="#" class="hi-icon hi-icon-cog glyphicon glyphicon-dashboard"></a></i>
-							</div>
-							<h4>Accusantium</h4>
-							<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
-						</div>
-						<div class="col-md-3 agile-3">
-							<div class="icon hi-icon-wrap hi-icon-effect-6">
-								<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-signal"></a>
-							</div>
-							<h4>Lorem ipsum</h4>
-							<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
-						</div>
-						<div class="col-md-3 agile-3">
-							<div class="icon hi-icon-wrap hi-icon-effect-6">
-								<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-cog"></a>
-							</div>
-							<h4>adipiscing elit</h4>
-							<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
-						</div>
-						<div class="col-md-3 agile-3">
-							<div class="icon hi-icon-wrap hi-icon-effect-6">
-								<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-file"></a>
-							</div>
-							<h4>Accusantium</h4>
-							<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
-						</div>
-						<div class="clearfix"></div>
-					</div>
+					<h4>Accusantium</h4>
+					<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
 				</div>
+				<div class="col-md-3 agile-3">
+					<div class="icon hi-icon-wrap hi-icon-effect-6">
+						<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-book"></a>
+					</div>
+					<h4>Lorem ipsum</h4>
+					<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
+				</div>
+				<div class="col-md-3 agile-3">
+					<div class="icon hi-icon-wrap hi-icon-effect-6">
+						<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-globe"></a>
+					</div>
+					<h4>adipiscing elit</h4>
+					<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
+				</div>
+				<div class="col-md-3 agile-3">
+					<div class="icon hi-icon-wrap hi-icon-effect-6">
+						<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-wrench"></a>
+					</div>
+					<h4>Accusantium</h4>
+					<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
+				</div>
+				<div class="clearfix"></div>
 			</div>
-<!--/ w3l-1 -->
+			<div class="featured-grids w3l-2">
+				<div class="col-md-3 agile-3">
+					<div class="icon hi-icon-wrap hi-icon-effect-6">
+						<i><a href="#" class="hi-icon hi-icon-cog glyphicon glyphicon-dashboard"></a></i>
+					</div>
+					<h4>Accusantium</h4>
+					<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
+				</div>
+				<div class="col-md-3 agile-3">
+					<div class="icon hi-icon-wrap hi-icon-effect-6">
+						<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-signal"></a>
+					</div>
+					<h4>Lorem ipsum</h4>
+					<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
+				</div>
+				<div class="col-md-3 agile-3">
+					<div class="icon hi-icon-wrap hi-icon-effect-6">
+						<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-cog"></a>
+					</div>
+					<h4>adipiscing elit</h4>
+					<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
+				</div>
+				<div class="col-md-3 agile-3">
+					<div class="icon hi-icon-wrap hi-icon-effect-6">
+						<a href="#" class="hi-icon hi-icon-support glyphicon glyphicon-file"></a>
+					</div>
+					<h4>Accusantium</h4>
+					<p>Quisque varius, nibh ac feugiat interdum, libero massa laoreet tellus, nec congue lorem arcu a nunc.</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	<!--/ w3l-1 -->
 
-<!-- footer -->
-<?php include("includes/footer.php"); ?>
-<!-- footer -->
+	<!-- footer -->
+	<?php include("includes/footer.php"); ?>
+	<!-- footer -->
 </body>
 <!-- //Body -->
+
 </html>
