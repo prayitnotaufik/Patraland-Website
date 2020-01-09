207e641b3b3990dd220b99e092d8700d303b508c
Nurhidayat Aldila <nurhidayat.aldila@gmail.com>
2020-01-09 13:28:57 +0700

    Layouting News pt2

---
+++ adm/news.php
@@ -0,0 +1,99 @@
+<?php
+session_start();
+include("../config/conn.php");
+if(@$_SESSION['status'] != "login") {
+  echo "<script type='text/javascript'>alert('Login terlebih dahulu!');window.location.href='index.php';</script>";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script>document.getElementsByTagName("html")[0].className += " js";</script>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>PT. Patraland - Administrator</title>
+  <style>
+  table {
+    font-size:17px;
+  }
+  </style>
+</head>
+<body>
+  <?php include("includes/header.php"); ?><!-- .cd-main-header -->
+  <main class="cd-main-content">
+    <?php include("includes/navbar.php"); ?>
+    <div class="cd-content-wrapper">
+
+
+      <h2>Berita</h2></h2><br>
+      <p>
+        <table cellpadding="5" width="100%" border=1>
+          <tr style="background:#ddd; font-weight:bolder;">
+            <th align="left">Title</th>
+            <th align="left">Author</th>
+            <th align="left">Date</th>
+            <th align="left">Status</th>
+            <th colspan="2"></th>
+          </tr>
+          <?php
+          $no = 1;
+          $sql = "SELECT * FROM tb_news";
+          $hasil = mysqli_query($db,$sql);
+          while($data = mysqli_fetch_assoc($hasil)) {
+            ?>
+            <tr>
+              <td><?php echo $data['title']; ?></td>
+              <td><?php echo $data['author']; ?></td>
+              <td><?php echo $data['date']; ?></td>
+              <td><?php echo $data['status']; ?></td>
+              <td align="center"><br><a class="btn btn--primary btn--sm" style="background:#28a745;" href="?id=<?php echo $data['id_news']; ?>&act=edit">Edit</a><br><br></td>
+              <td align="center"><br><a class="btn btn--accent btn--sm" href="?id=<?php echo $data['id_news']; ?>&act=del">Hapus</a><br><br></td>
+            </tr>
+          <?php } ?>
+        </table>
+      </form>
+    </p>
+    <?php
+    if(@$_GET['act']=='del') {
+      $id = $_GET['id'];
+      $sqldel = "DELETE FROM tb_news WHERE id_news = '$id'";
+      $result = mysqli_query($db,$sqldel);
+      echo "<script type='text/javascript'>alert('Hapus data berhasil!');window.location.href='news.php';</script>";
+    }
+    ?>
+  </div> <!-- .content-wrapper -->
+</main> <!-- .cd-main-content -->
+<script src="assets/js/util.js"></script> <!-- util functions included in the CodyHouse framework -->
+<script src="assets/js/menu-aim.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
+
+<script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
--- news.php
+++ news.php
@@ -1,36 +1,41 @@
+<?php
+include("config/conn.php");
+$sql = "SELECT * FROM tb_news WHERE status = 'Published'";
+$result = mysqli_query($db,$sql);
+?>
 <!DOCTYPE html>
 <html>
 <!-- Head -->
 
 <head>
-	<title>PT. Patraland Griya Madani</title>
-	<link rel="shortcut icon" type="image/png" href="images/icon2.png">
-	<!-- Meta-Tags -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="keywords" content="Premier Realty a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
-	<script type="application/x-javascript">
-		addEventListener("load", function() {
-			setTimeout(hideURLbar, 0);
-		}, false);
+  <title>PT. Patraland Griya Madani</title>
+  <link rel="shortcut icon" type="image/png" href="images/icon2.png">
+  <!-- Meta-Tags -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="keywords" content="Premier Realty a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
+  <script type="application/x-javascript">
+  addEventListener("load", function() {
+    setTimeout(hideURLbar, 0);
+  }, false);
 
-		function hideURLbar() {
-			window.scrollTo(0, 1);
-		}
-	</script>
-	<!-- //Meta-Tags -->
-	<!-- Custom-Theme-Files -->
-	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/about.css" type="text/css" media="all">
-	<!-- //Custom-Theme-Files -->
-	<!-- Web-Fonts -->
-	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" type="text/css">
-	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat:400,700" type="text/css">
-	<!-- //Web-Fonts -->
-	<!-- Default-JavaScript-File -->
-	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+  function hideURLbar() {
+    window.scrollTo(0, 1);
+  }
+  </script>
+  <!-- //Meta-Tags -->
+  <!-- Custom-Theme-Files -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/about.css" type="text/css" media="all">
+  <!-- //Custom-Theme-Files -->
+  <!-- Web-Fonts -->
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" type="text/css">
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat:400,700" type="text/css">
+  <!-- //Web-Fonts -->
+  <!-- Default-JavaScript-File -->
+  <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <style>
   .bts {
     clear:both;
@@ -43,41 +48,44 @@
 <!-- Body -->
 
 <body>
-	<!-- Header -->
-	<div class="header">
-		<!-- Top-Bar -->
-		<?php include("includes/topbar.php"); ?>
-		<!-- //Top-Bar -->
-		<div class="banner">
-			<div class="bann-info">
-			</div>
-		</div>
-		<!-- //Slider -->
-	</div>
-	<!-- //Header -->
-	<div class="about-bottom wthree-3">
-		<div class="container">
-			<h2 class="tittle">Berita</h2>
+  <!-- Header -->
+  <div class="header">
+    <!-- Top-Bar -->
+    <?php include("includes/topbar.php"); ?>
+    <!-- //Top-Bar -->
+    <div class="banner">
+      <div class="bann-info">
+      </div>
+    </div>
+    <!-- //Slider -->
+  </div>
+  <!-- //Header -->
+  <div class="about-bottom wthree-3">
+    <div class="container">
+      <h2 class="tittle">Berita</h2>
       <!-- While -->
-			<div class="agileinfo_about_bottom_grids">
-				<div class="col-md-6 agileinfo_about_bottom_grid">
-          <h5><b>Patraland Place (Hunian Berkelas di Lokasi Strategis)</b></h5>
-					<p>PT. Patraland Griya Madani Development menawarkan sebuah hunian pilihan yang sangat sesuai untuk Anda yang ingin setiap saat menikmati keindahan dan kesejukan kota Malang. Perumahan Patraland adalah proyek perumahan dengan pemilihan lokasi yang sangat strategis, dan telah melalui pertimbangan yang matang demi kepuasan penghuninya.</p>
-          <small><i>(Posted by <u>Patraland</u> - <?php echo date("d / m / Y"); ?>)</i></small>
-				</div>
-				<div class="col-md-6 agileinfo_about_bottom_grid">
-					<img src="images/slide-5.jpg" alt=" " class="img-responsive">
-				</div>
-			</div>
-      <div class="bts">&nbsp;</div>
-      <!-- End While -->
-		</div>
-	</div>
+      <?php while($data = mysqli_fetch_assoc($result)) { ?>
+        <div class="agileinfo_about_bottom_grids">
+          <div class="col-md-6 agileinfo_about_bottom_grid">
+            <h5><b><a href="" style="color:black;"><?php echo $data['title']; ?></a></b></h5>
+            <div style="position:inherit"><small><i>(Posted by <u><?php echo $data['author']; ?></u> - <?php echo $data['date']; ?>)</i></small></div>
+            <p><?php echo substr($data['description'], 0, 400); ?> ... - <a href=""><u>Baca Selengkapnya</u></a></p>
 
-	<!--/ w3l-1 -->
-	<!-- footer -->
-	<?php include("includes/footer.php"); ?>
-	<!-- footer -->
+          </div>
+          <div class="col-md-6 agileinfo_about_bottom_grid">
+            <img src="images/news/<?php echo $data['image']; ?>" alt=" " class="img-responsive" style="width:80%; border:10px solid #ddd;">
+          </div>
+        </div>
+        <div class="bts">&nbsp;</div>
+      <?php } ?>
+      <!-- End While -->
+    </div>
+  </div>
+
+  <!--/ w3l-1 -->
+  <!-- footer -->
+  <?php include("includes/footer.php"); ?>
+  <!-- footer -->
 </body>
 <!-- //Body -->
 
